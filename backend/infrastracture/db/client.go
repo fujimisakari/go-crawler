f6e12f2f23216ea3d9e6026a8e0fce4f022a2acf
Fujimisakari <fujimisakari@gmail.com>
2018-01-08 15:31:39 +0900

    Modify db config

--- backend/infrastracture/db/client.go
+++ backend/infrastracture/db/client.go
@@ -2,13 +2,13 @@ package db
 
 import (
 	"database/sql"
-	// "io/ioutil"
-	// "os"
-	// "path/filepath"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	// mysql is used in database connection, it is slient include
 	_ "github.com/go-sql-driver/mysql"
-	// "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 )
 
 // Database has database connection
@@ -20,32 +20,28 @@ var sharedInstance = New()
 
 // New is create Database object with connection pool
 func New() *Database {
-	// env := os.Getenv("APPENV")
-	// root := os.Getenv("APPROOT")
-	// env := "development"
-	// root := "../config/"
-	// path := filepath.Join(root, "db.yml")
-	// buf, err := ioutil.ReadFile(path)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// m := make(map[interface{}]interface{})
-	// err = yaml.Unmarshal(buf, &m)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// username := m[env].(map[interface{}]interface{})["user"].(string)
-	// password := m[env].(map[interface{}]interface{})["password"].(string)
-	// database := m[env].(map[interface{}]interface{})["name"].(string)
-	// host := m[env].(map[interface{}]interface{})["host"].(string)
-	// pool := m[env].(map[interface{}]interface{})["pool"].(int)
-	// username = os.ExpandEnv(username)
-	// password = os.ExpandEnv(password)
-	// database = os.ExpandEnv(database)
-	// host = os.ExpandEnv(host)
-	// db, err := sql.Open("mysql", username+":"+password+"@tcp("+host+":3306)/"+database+"?parseTime=true&charset=utf8mb4")
-	pool := 5
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/crawler?parseTime=true&charset=utf8mb4")
+	env := os.Getenv("APPENV")
+	root := os.Getenv("APPROOT")
+	path := filepath.Join(root, "backend/config/db.yml")
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(buf, &m)
+	if err != nil {
+		panic(err)
+	}
+	username := m[env].(map[interface{}]interface{})["user"].(string)
+	password := m[env].(map[interface{}]interface{})["password"].(string)
+	database := m[env].(map[interface{}]interface{})["name"].(string)
+	host := m[env].(map[interface{}]interface{})["host"].(string)
+	pool := m[env].(map[interface{}]interface{})["pool"].(int)
+	username = os.ExpandEnv(username)
+	password = os.ExpandEnv(password)
+	database = os.ExpandEnv(database)
+	host = os.ExpandEnv(host)
+	db, err := sql.Open("mysql", username+":"+password+"@tcp("+host+":3306)/"+database+"?parseTime=true&charset=utf8")
 	if err != nil {
 		panic(err)
 	}
